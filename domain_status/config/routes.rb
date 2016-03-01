cdfe40f9e3a3838e50de45f4c6640c58d80a14d1
Anita Pacheco <anita@MacBook-Pro-de-Anita.local>
2016-03-01 04:01:59 -0300

    borrando ruta de prueba

--- domain_status/config/routes.rb
+++ domain_status/config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
 
   resources :principal, path: 'status.:domain', action: 'status', :constraints => { :domain => /.+/ }
-  resources :principal, path: 'status/test', action: 'status_test'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
